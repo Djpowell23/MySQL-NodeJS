fac60e824d60852f251bf18cb818a305d099aef7
Devin Powell <dpowel11@kent.edu>
2019-06-02 13:43:27 -0600

    initial setup, packages installed

---
+++ bamazonCustomer.js
@@ -0,0 +1,20 @@
+// Requirements
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
+// Connect to mysql
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Goober23.',
+    database: 'bamazon'
+});
+
+// Once Server is connected, Main Code Goes Here
+connection.connect(function(err){
+    if (err) throw err;
+    console.log('Connected to mysql database');
+})
+
+// Functions
