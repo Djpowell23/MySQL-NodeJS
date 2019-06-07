03b447caf14c50d536908ca6800cc9593781ad65
Devin Powell <dpowel11@kent.edu>
2019-06-07 14:59:56 -0600

    Add New Product function complete

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,9 +1,9 @@
 // List a set of menu options:
-// View Products for Sale
+// View Products for Sale               // done
 
-// View Low Inventory
+// View Low Inventory                   // done
 
-// Add to Inventory
+// Add to Inventory                    
 
 // Add New Product
 
@@ -26,7 +26,7 @@ var connection = mysql.createConnection({
     database: 'bamazon'
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
 
     manage();
@@ -37,27 +37,27 @@ function manage() {
         {
             type: 'list',
             name: 'managerChoice',
-            choices: ['View Products for Sale','View Low Inventory','Add New Inventory','Add New Product','Leave Shop']
+            choices: ['View Products for Sale', 'View Low Inventory', 'Add New Inventory', 'Add New Product', 'Leave Shop']
         }
-    ]).then(function(answer) {
+    ]).then(function (answer) {
         switch (answer.managerChoice) {
             case ('View Products for Sale'):
                 viewProduct();
-            break;
+                break;
             case ('View Low Inventory'):
                 viewLowInv();
-            break;
+                break;
 
             case ('Add New Inventory'):
                 addInventory();
-            break;
+                break;
             case ('Add New Product'):
                 addProduct();
-            break;
+                break;
             case ('Leave Shop'):
                 console.log('Have a nice day, sir!');
                 connection.end();
-            break;
+                break;
 
 
         }
@@ -66,18 +66,18 @@ function manage() {
 
 // Display all information about current products
 function viewProduct() {
-    connection.query('SELECT * FROM products', function(err, res) {
+    connection.query('SELECT * FROM products', function (err, res) {
         if (err) throw err;
-        console.table(res, ['item_id','product_name','department_name','price','stock_quantity']);
+        console.table(res, ['item_id', 'product_name', 'department_name', 'price', 'stock_quantity']);
         manage();
     });
 }
 
 // Display Contents that are running low on inventory
 function viewLowInv() {
-    connection.query('SELECT * FROM products WHERE stock_quantity < 25', function(err,res){
-        console.table(res, ['item_id','product_name','department_name','price','stock_quantity']);
-        
+    connection.query('SELECT * FROM products WHERE stock_quantity < 25', function (err, res) {
+        console.table(res, ['item_id', 'product_name', 'department_name', 'price', 'stock_quantity']);
+
         // If no items are populated:
         if (res[0].item_id === undefined) {
             console.log('No products are low inventory status.');
@@ -91,8 +91,42 @@ function viewLowInv() {
 
 function addInventory() {
 
+
 }
 
 function addProduct() {
+    inquirer.prompt([
+        {
+            name: 'productName',
+            type: 'input',
+            message: 'What is the product_name?'
+        },
+        {
+            name: 'departmentName',
+            type: 'input',
+            message: 'What department does it belong to?'
+        },
+        {
+            name: 'price',
+            type: 'input',
+            message: 'How much for the price of one unit?'
+        },
+        {
+            name: 'stockQuantity',
+            type: 'input',
+            message: 'How many units are you adding to inventory?'
+        }
+    ]).then(function (answer) {
+        connection.query(`INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('${answer.productName}', '${answer.departmentName}', '${answer.price}', '${answer.stockQuantity}')`,
+            function (err) {
+                if (err) throw err;
 
+                console.log('===============================');
+                console.log(`Added ${answer.productName} to inventory!`);
+                console.log('===============================');
+                viewProduct();
+                manage();
+            });
+        });
+        
 }
